94d0aef9cabab923b17fc883e4e17803a7c5cb33
Christopher Dent <christopherdent01@gmail.com>
2019-11-16 01:35:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,10 +15,14 @@ class Application
       item = @@items.detect { |i| i.name == item_name }
       if item
         resp.write item.price
+             else
+        resp.write "Item not found"
+        resp.status = 400
+      end 
       else
         resp.write "Route not found"
         resp.status = 404
-      end
+       
     end 
     resp.finish
   end
